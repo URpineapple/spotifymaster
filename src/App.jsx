d8dcad155cced646ce72b42d79a687960a88fed5
URpineapple <zhang.te@husky.neu.edu>
2020-04-11 16:41:53 -0400

    Added extra functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,57 @@
 import React, { Component } from 'react'
 import './App.css'
 import { FormGroup, InputGroup, FormControl } from 'react-bootstrap'
+import Profile from './Profile'
+import queryString from 'query-string'
+import Gallery from './Gallery'
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            query: ''
+            query: '',
+            artist: null,
+            tracks: [],
+            accessToken: ''
         }
+    }
+
+    componentDidMount() {
+        const parsed = queryString.parse(window.location.search);
+        this.setState({
+            accessToken: parsed.access_token
+        })
+        let token = this.state.accessToken
+        if (!token)
+            return;
+        else
+            console.log('accesstoken', token)
     }
 
     search() {
         const BASE_URL = "https://api.spotify.com/v1/search?"
-        const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`
-        console.log('this.state', this.state)
-        console.log('FETCH_URL', FETCH_URL)
+        let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`
+        const ALBUM_URL = "https://api.spotify.com/v1/artists/"
+
+        fetch(FETCH_URL, {
+            headers: { 'Authorization': 'Bearer ' + this.state.accessToken },
+            method: 'GET'
+        }).then(response => response.json())
+            .then(json => {
+                const artist = json.artists.items[0]
+                this.setState({ artist })
+
+                FETCH_URL = `${ALBUM_URL}${artist.id}/top-tracks?country=US&`
+                fetch(FETCH_URL, {
+                    headers: { 'Authorization': 'Bearer ' + this.state.accessToken },
+                    method: 'GET'
+                }).then(response => response.json())
+                    .then(json => {
+                        console.log('artist\'s top tracks', json)
+                        const { tracks } = json
+                        this.setState({ tracks })
+                    })
+            })
     }
 
     render() {
@@ -36,11 +73,8 @@ class App extends Component {
                     </InputGroup>
 
                 </FormGroup>
-                <div className="Profile">
-                    <div>Artist Profile</div>
-                    <div>Artist Name</div>
-                </div>
-                <div className="gallery">Gallery</div>
+                {this.state.artist && <Profile artist={this.state.artist} />}
+                <Gallery tracks={this.state.tracks} />
             </div>
         )
     }
---
+++ src/Gallery.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+
+export default class Gallery extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            playingUrl: '',
+            audio: null,
+            playing: false
+        }
+    }
+
+    playAudio(previewUrl) {
+        console.log('previewUrl', previewUrl)
+        let audio = new Audio(previewUrl)
+        if (!this.state.playing) {
+            audio.play()
+            this.setState({
+                playing: true,
+                playingUrl: previewUrl,
+                audio
+            })
+        } else {
+            if (this.state.playingUrl == previewUrl) {
+                this.state.audio.pause()
+                this.setState({
+                    playing: false,
+                    playingUrl: null
+                })
+            } else {
+                this.state.audio.pause()
+                audio.play()
+                this.setState({
+                    playingUrl: previewUrl,
+                    playing: true,
+                    audio
+                })
+            }
+        }
+
+    }
+    render() {
+        console.log('gallery.props', this.props)
+        const { tracks } = this.props
+        return (
+            <div className="gallery">
+                {
+                    tracks.map((track, index) => {
+                        const trackImg = track.album.images[0].url;
+                        return (
+                            <div key={index} className="track" onClick={() => this.playAudio(track.preview_url)}>
+                                <img src={trackImg} alt="track" className="track-img" />
+                                <div className="track-play">
+                                    <div className="track-play-inner">
+                                        {this.state.playingUrl == track.preview_url
+                                            ? <span>| |</span>
+                                            : <span><i class="fa fa-play"></i></span>
+                                        }
+                                    </div>
+                                </div>
+                                <p className="track-text">{track.name}</p>
+                            </div>
+                        )
+
+                    })
+                }
+            </div>
+        )
+    }
+}
