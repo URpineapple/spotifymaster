94ce33ed16741414586ea341d1a757ef9e0830e7
tengzhang <ztengc5@gmail.com>
2023-05-22 16:23:40 -0400

    add build files

---
+++ build/precache-manifest.a1edf96b622459e7f83938f694ad7966.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb87c99f87e6e5b9185d2dfb6e103f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a66854319d49fb865f62",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "4cfaf6dec827150b3a1f",
+    "url": "/static/css/main.a08cde2c.chunk.css"
+  },
+  {
+    "revision": "a66854319d49fb865f62",
+    "url": "/static/js/2.da1de5e6.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.da1de5e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cfaf6dec827150b3a1f",
+    "url": "/static/js/main.41bd6ac4.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.41bd6ac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd172b8b2c911af60ad3",
+    "url": "/static/js/runtime-main.7f786fb6.js"
+  }
+]);
